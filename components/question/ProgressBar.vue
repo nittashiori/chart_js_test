0a7306c28dee11f2af9741ee00f1a353fb897335
nittashiori <nitta@impv.co.jp>
2020-07-10 15:14:49 +0900

    プログレスバー（数値）追加

---
+++ components/question/ProgressBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <p class="progressbar">{{ count }}/{{ jsonMaxNum }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      require: true,
+      default() {
+        return this.$store.getters['questions/error']
+      },
+    },
+    jsonMaxNum: {
+      type: Number,
+      require: true,
+      default() {
+        return this.$store.getters['questions/getQuestions'].length
+      },
+    },
+  },
+  data() {
+    return {
+      count: Number(this.data.id),
+    }
+  },
+}
+</script>
